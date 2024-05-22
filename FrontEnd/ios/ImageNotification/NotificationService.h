ecef75a8b72ebbb3f81caec2a734316451e2323b
Arijit Sarkar <asarkar@retailershakti.com>
2024-05-22 15:39:00 +0530

    update

---
+++ FrontEnd/ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Dip Bagchi on 09/05/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
