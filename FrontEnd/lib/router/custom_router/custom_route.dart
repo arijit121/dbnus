0955d37ec68420f05809d144ce5518b2ad4e9290
Arijit Sarkar <asarkar@retailershakti.com>
2024-07-08 12:57:29 +0530

    update clearAndNavigate

--- FrontEnd/lib/router/custom_router/custom_route.dart
+++ FrontEnd/lib/router/custom_router/custom_route.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../extension/logger_extension.dart';
+import '../../service/JsService/provider/js_provider.dart';
 import '../../service/context_service.dart';
 import '../router_manager.dart';
 import '../router_name.dart';
@@ -46,15 +47,48 @@ class CustomRoute {
           pathParameters: pathParameters,
           extra: extra);
     } else {
-      if (CustomRouterWeb().historyIndex() != 0) {
-        CustomRouterWeb().numBack(CustomRouterWeb().historyIndex());
+      RouterManager routerManager = RouterManager.getInstance;
+      RouteBase routeBase = routerManager.router.configuration.routes
+          .firstWhere((element) => element.toString().contains(name),
+              orElse: () {
+        return routerManager.router.configuration.routes.first;
+      });
+      // GoRoute#dda94(name: "/return_order", path: "/return_order/:orderID/:invoiceID")
+      // [GoRoute#e5eb9(name: "/return_order",  path: "/return_order/:orderID/:invoiceID")]
+      List<String> res = routeBase.toString().split(",");
+      String res2 = res.last.replaceAll(")", "");
+      String res3 = res2.replaceAll("path:", "").trim();
+      String res4 = res3.replaceAll('"', "").trim();
+
+      String url = res4;
+
+      if (queryParameters.isNotEmpty) {
+        url = Uri.parse(name)
+            .replace(queryParameters: queryParameters)
+            .toString();
       }
-      Future.delayed(
-          Duration(milliseconds: CustomRouterWeb().historyIndex() * 10), () {
-        CustomRouterWeb().goToNameAndOff(name,
-            queryParameters: queryParameters,
-            pathParameters: pathParameters,
-            extra: extra);
+
+      if (pathParameters.isNotEmpty && res3.contains("/:")) {
+        String temp = url;
+
+        List<String> keyList = pathParameters.keys.toList();
+        List<String> valueList = pathParameters.values.toList();
+        url = Uri.parse(temp.replaceAll(
+                "/:${keyList.join("/:")}", "/${valueList.join("/")}"))
+            .toString();
+      }
+      Router.neglect(CurrentContext().context, () async {
+        if (CustomRouterWeb().historyIndex() != 0) {
+          CustomRouterWeb().numBack(CustomRouterWeb().historyIndex());
+        }
+        await Future.delayed(
+            Duration(milliseconds: CustomRouterWeb().historyIndex() * 10), () {
+          CustomRouterWeb().goToNameAndOff(name,
+              queryParameters: queryParameters,
+              pathParameters: pathParameters,
+              extra: extra);
+        });
+        JsProvider().changeUrl(path: url);
       });
     }
   }
