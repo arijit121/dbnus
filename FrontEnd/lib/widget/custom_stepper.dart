9253357fbe68a98b87dd7b7e02ef405d5e958a7c
Arijit Sarkar <asarkar@retailershakti.com>
2024-07-16 17:19:28 +0530

    update CustomHorizontalStepper

--- FrontEnd/lib/widget/custom_stepper.dart
+++ FrontEnd/lib/widget/custom_stepper.dart
@@ -62,6 +62,71 @@ class CustomVerticalStepper extends StatelessWidget {
   }
 }
 
+class CustomHorizontalStepper extends StatelessWidget {
+  final List<CustomStepperContent>? customStepperContent;
+  final double? lineHeight, lineWidth;
+  final ScrollPhysics? physics;
+  final bool shrinkWrap;
+  final Color activeLineColor;
+  final Color inActiveLineColor;
+
+  const CustomHorizontalStepper({
+    super.key,
+    this.customStepperContent,
+    this.physics,
+    this.lineHeight,
+    this.lineWidth = 30,
+    this.shrinkWrap = false,
+    this.activeLineColor = Colors.green,
+    this.inActiveLineColor = Colors.grey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        physics: physics,
+        shrinkWrap: shrinkWrap,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          CustomStepperContent? content =
+              customStepperContent?.elementAt(index);
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                      width: (lineWidth ?? 0.1) / 2,
+                      height: lineHeight ?? 2,
+                      color: index == 0
+                          ? Colors.transparent
+                          : content?.isActive == true
+                              ? activeLineColor
+                              : inActiveLineColor),
+                  if (content?.leading != null) content!.leading,
+                  Container(
+                      width: (lineWidth ?? 0.1) / 2,
+                      height: lineHeight ?? 2,
+                      color: (index + 1) == customStepperContent?.length
+                          ? Colors.transparent
+                          : content?.isActive == true
+                              ? activeLineColor
+                              : inActiveLineColor)
+                ],
+              ),
+              if (content?.content != null)
+                SizedBox(
+                    width: (lineWidth ?? 0),
+                    child: Center(child: content!.content)),
+            ],
+          );
+        },
+        separatorBuilder: (context, index) {
+          return 0.pw;
+        },
+        itemCount: customStepperContent?.length ?? 0);
+  }
+}
+
 class CustomStepperContent {
   CustomStepperContent(
       {required this.isActive, required this.content, required this.leading});
