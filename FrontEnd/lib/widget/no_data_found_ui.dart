41c11b9edef2ca9a959eb8f1a2f3a13ce44e4899
Arijit Sarkar <asarkar@retailershakti.com>
2024-08-02 18:11:34 +0530

    update NoDataFound

--- FrontEnd/lib/widget/no_data_found_ui.dart
+++ FrontEnd/lib/widget/no_data_found_ui.dart
@@ -1,34 +1,52 @@
 import 'package:dbnus/extension/spacing.dart';
+import 'package:dbnus/widget/custom_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 import '../const/assects_const.dart';
 import '../service/value_handler.dart';
-import '../utils/screen_utils.dart';
 import 'custom_text.dart';
 
 class NoDataFound extends StatelessWidget {
-  final String? msg;
-  final Widget? widget;
+  const NoDataFound(
+      {super.key,
+      this.height = 300,
+      this.width = 300,
+      this.backgroundColor = Colors.transparent,
+      this.msg});
 
-  const NoDataFound({super.key, this.msg, this.widget});
+  final double width;
+  final double height;
+  final Color? backgroundColor;
+  final String? msg;
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: SizedBox(
-        width: ScreenUtils.aw(),
+      child: Container(
+        color: backgroundColor,
+        width: width,
+        height: height,
+        padding: const EdgeInsets.symmetric(vertical: 10),
         child: Column(
           children: [
             30.ph,
-            SvgPicture.asset(
-              AssetsConst.emptyFile,
-              allowDrawingOutsideViewBox: true,
+            CustomAssetImageView(
+              path: AssetsConst.noRecordFound,
+              // width: width - 20,
+              height: height -
+                  (80 +
+                      44 +
+                      (ValueHandler().isTextNotEmptyOrNull(msg) ? 20 : 0)),
+            ),
+            const CustomText(
+              "No record found",
+              color: Colors.blue,
+              size: 36,
+              fontWeight: FontWeight.w600,
             ),
             30.ph,
             if (ValueHandler().isTextNotEmptyOrNull(msg))
               CustomText(msg ?? "", color: Colors.blue, size: 20),
-            if (widget != null) ...[5.ph, widget!],
           ],
         ),
       ),
