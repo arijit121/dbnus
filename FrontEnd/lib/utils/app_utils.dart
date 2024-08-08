765a98733e6c58bdfd6d67b0a25c9452bebf927d
Arijit Sarkar <asarkar@retailershakti.com>
2024-08-08 12:22:45 +0530

    update

--- FrontEnd/lib/utils/app_utils.dart
+++ FrontEnd/lib/utils/app_utils.dart
@@ -1,17 +1,15 @@
 import 'screen_utils.dart';
 
 class AppUtils {
-  int gridViewCount({required double elementWidth, double? extra}) {
-    double valExtra = extra ?? 0;
+  int gridViewCount({required num elementWidth, num? extra}) {
+    num valExtra = extra ?? 0;
     return ((ScreenUtils.nw() - valExtra) ~/ elementWidth) > 1
         ? ((ScreenUtils.nw() - valExtra) ~/ elementWidth)
         : 1;
   }
 
   double gridViewRation(
-      {required double elementWidth,
-      required double elementHeight,
-      double? extra}) {
+      {required num elementWidth, required num elementHeight, num? extra}) {
     int valGridViewCount =
         gridViewCount(elementWidth: elementWidth, extra: extra);
     return (ScreenUtils.nw() / valGridViewCount) / elementHeight;
