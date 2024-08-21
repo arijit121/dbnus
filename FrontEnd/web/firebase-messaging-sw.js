d92bc4033fa3ed7190ceedba2175edc31d53c1de
Arijit <ruarijitsarkar@gmail.com>
2024-08-21 23:07:45 +0530

    update fcm web

--- FrontEnd/web/firebase-messaging-sw.js
+++ FrontEnd/web/firebase-messaging-sw.js
@@ -14,9 +14,61 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
+let messaging;
+try {
+  messaging = firebase.messaging.isSupported() ? firebase.messaging() : null
+} catch (err) {
+  console.error('Failed to initialize Firebase Messaging', err);
+}
 
-// Optional:
-messaging.onBackgroundMessage((message) => {
-  console.log("onBackgroundMessage", message.data);
+if (messaging) {
+  // To dispaly background notifications
+  try {
+    messaging.onBackgroundMessage((message) => {
+      console.log('Received background message: ', message.data);
+      var data = message.data;
+      var title = data['title'];
+      var body = data['body'];
+      var bigText = data['message'];
+      var image = data['image'];
+      var actionURL = data['ActionURL'];
+      var notificationOptions = {
+        tag: body,
+        body: bigText,
+        icon: image,
+        data: {
+          url: actionURL,// This should contain the URL you want to open
+        },
+      }
+      self.registration.showNotification(title, notificationOptions);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// File: firebase-messaging-sw.js
+// Handling Notification click
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close(); // CLosing the notification when clicked
+  console.log('Notification click: ', event.data);
+  const urlToOpen = event?.notification?.data?.url || window.location.origin;
+  // Open the URL in the default browser.
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window',
+    })
+      .then((windowClients) => {
+        // Check if there is already a window/tab open with the target URL
+        for (const client of windowClients) {
+          if (client.url === urlToOpen && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        // If not, open a new window/tab with the target URL
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen);
+        }
+      })
+  );
 });
