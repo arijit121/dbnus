96e59c86507ab587b3659929dec2da9ced1d7627
Arijit <ruarijitsarkar@gmail.com>
2024-08-24 23:30:09 +0530

    update

--- FrontEnd/lib/service/redirect_engine.dart
+++ FrontEnd/lib/service/redirect_engine.dart
@@ -10,15 +10,15 @@ class RedirectEngine {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
       Future.delayed(Duration(seconds: delayedSeconds), () async {
         RouterManager routerManager = RouterManager.getInstance;
-        String fragment =
+        String location =
             redirectUrl.toString().replaceAll(ApiUrlConst.hostUrl, "");
         if (routerManager.router.routeInformationParser.configuration
-            .findMatch(fragment)
+            .findMatch(Uri(path: redirectUrl.path))
             .matches
             .isNotEmpty) {
           kIsWeb
-              ? routerManager.router.go(fragment)
-              : routerManager.router.push(fragment);
+              ? routerManager.router.go(location)
+              : routerManager.router.push(location);
         }
       });
     });
