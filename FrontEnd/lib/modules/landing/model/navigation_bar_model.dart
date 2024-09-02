823eff8dfa10accd17c2405413440efc4b42e993
Arijit <ruarijitsarkar@gmail.com>
2024-09-02 23:22:31 +0530

    update

---
+++ FrontEnd/lib/modules/landing/model/navigation_bar_model.dart
@@ -0,0 +1,7 @@
+class NavigationBarModel {
+  final String title;
+  final String icon;
+  final String action;
+  const NavigationBarModel(
+      {required this.title, required this.icon, required this.action});
+}
