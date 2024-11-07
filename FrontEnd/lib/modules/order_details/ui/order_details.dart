39f1d8bde6aff4fae7f7ced32244d66732431474
Arijit <ruarijitsarkar@gmail.com>
2024-11-08 02:15:58 +0530

    update

---
+++ FrontEnd/lib/modules/order_details/ui/order_details.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class OrderDetails extends StatelessWidget {
+  const OrderDetails({super.key, required this.orderId});
+  final String orderId;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ FrontEnd/lib/modules/settings/ui/settings.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({super.key, this.accountSetting});
+  final bool? accountSetting;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
