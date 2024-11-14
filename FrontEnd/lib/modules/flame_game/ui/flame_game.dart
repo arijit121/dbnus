5d834a128aa9af0d0ac6485644b400e0c00ca42e
Arijit <ruarijitsarkar@gmail.com>
2024-11-14 22:44:56 +0530

    update

--- FrontEnd/lib/modules/flame_game/ui/flame_game.dart
+++ FrontEnd/lib/modules/flame_game/ui/flame_game.dart
@@ -1,8 +1,9 @@
 import 'package:dbnus/utils/screen_utils.dart';
 import 'package:flutter/material.dart';
 
-import '../widget/boat_game.dart';
 import 'package:flame/game.dart';
+
+import '../widget/scrolling_background_game.dart';
 
 class FlameGame extends StatelessWidget {
   const FlameGame({super.key});
@@ -10,8 +11,8 @@ class FlameGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveBuilder(builder: (context, widthState) {
-      return GameWidget(
-          game: BoatGame(
+      return GameWidgetWithGesture(
+          game: SingleScrollBackgroundGame(
         widthState: widthState,
       ));
     });
