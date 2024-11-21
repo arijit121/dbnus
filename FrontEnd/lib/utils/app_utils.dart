f42e0a74005740c2fd626d8a2e1f0d1245015447
Arijit Sarkar <asarkar@retailershakti.com>
2024-11-21 15:16:27 +0530

    update

--- FrontEnd/lib/utils/app_utils.dart
+++ FrontEnd/lib/utils/app_utils.dart
@@ -8,36 +8,34 @@ import 'pop_up_items.dart';
 import 'screen_utils.dart';
 
 class AppUtils {
-  Future<Position?> getCurrentPosition() async {
+  Future<Position?> getCurrentPosition({bool? handleDeniedForever}) async {
     try {
       LocationPermission permission;
       permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
         permission = await Geolocator.requestPermission();
         if (permission == LocationPermission.denied) {
-          return Future.error('Location permissions are denied');
+          return Future.error('Location permissions are denied.');
         }
       }
       if (permission == LocationPermission.deniedForever) {
-        if (kIsWeb) {
+        if (kIsWeb || handleDeniedForever != true) {
           permission = await Geolocator.requestPermission();
         } else {
           await Geolocator.openAppSettings();
           permission = await Geolocator.checkPermission();
-          if (permission == LocationPermission.deniedForever) {
-            return Future.error(
-                'Location permissions are permanently denied, we cannot request permissions.');
-          }
+        }
+        if (permission == LocationPermission.deniedForever) {
+          return Future.error(
+              'Location permissions are permanently denied, we cannot request permissions.');
         }
       }
 
       return await Geolocator.getCurrentPosition();
     } catch (e, stacktrace) {
       AppLog.e(e.toString(), error: e, stackTrace: stacktrace);
-      PopUpItems()
-          .toastMessage(e.toString(), ColorConst.red, durationSeconds: 4);
+      return Future.error(e, stacktrace);
     }
-    return null;
   }
 
   int gridViewCount(
