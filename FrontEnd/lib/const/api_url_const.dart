4fc73ef593091c7d2d971707ba50affa2083668c
animesh <abanerjee2@sastasundar.com>
2024-12-26 17:14:58 +0530

    update

--- FrontEnd/lib/const/api_url_const.dart
+++ FrontEnd/lib/const/api_url_const.dart
@@ -1,8 +1,13 @@
+import '../flavors.dart';
+
 class ApiUrlConst {
-  static bool isLive = true;
-  static String baseUrl = isLive
+  static String baseUrl = F.appFlavor == Flavor.prod
       ? "https://www.googleapis.com/"
-      : "https://www.stg-googleapis.com/";
+      : F.appFlavor == Flavor.stg
+          ? "https://www.stg-googleapis.com/"
+          : F.appFlavor == Flavor.dev
+              ? "https://www.dev-googleapis.com/"
+              : "https://www.googleapis.com/";
 
   static String books = "${baseUrl}books/v1/volumes";
   static String hostUrl = "https://www.google.com";
