bf46cb85f75054b58cd894b8d510536a279f0e80
Arijit <ruarijitsarkar@gmail.com>
2025-05-07 23:31:21 +0530

    update

--- FrontEnd/android/app/build.gradle.kts
+++ FrontEnd/android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.arj.dbnus"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
