610fdcc135736e455931447c8020e11d4eee50a2
Arijit <ruarijitsarkar@gmail.com>
2025-08-06 01:42:09 +0530

    update

--- FrontEnd/lib/modules/ui_temp/ui/ui_temp.dart
+++ FrontEnd/lib/modules/ui_temp/ui/ui_temp.dart
@@ -185,6 +185,7 @@ class _UiTempState extends State<UiTemp> {
                         controller: _pinController,
                         length: 5,
                         autoFocus: true,
+                        autoFill: true,
                         onCompleted: (value) {
                           AppLog.i(_pinController.text);
                         },
