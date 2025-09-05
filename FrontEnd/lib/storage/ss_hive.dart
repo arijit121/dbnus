cee9b743ed4ac39362f8ad947ae17ece38ce499d
Arijit Sarkar <asarkar@retailershakti.com>
2025-09-05 11:37:54 +0530

    update

--- FrontEnd/lib/storage/ss_hive.dart
+++ FrontEnd/lib/storage/ss_hive.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' deferred as path_provider;
 
+import '../config/app_config.dart';
 import '../service/value_handler.dart' deferred as value_handler;
 import 'local_preferences.dart' deferred as local_preferences;
 
@@ -47,8 +48,10 @@ class SsHive {
       storePath =
           "${(await path_provider.getApplicationDocumentsDirectory()).path}/hive_box";
     }
+    String appVersion = (await AppConfig().getAppVersion()) ?? "1.0.0";
+    String appVersionCode = (await AppConfig().getAppVersionCode()) ?? "1";
     BoxCollection collection = await BoxCollection.open(
-      _collectionName,
+      "$_collectionName-$appVersion-$appVersionCode",
       _boxNames,
       path: storePath,
       key: HiveAesCipher(key),
