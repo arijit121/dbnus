6d8bae1af161614ce728554e3e77d3c5f8221ad3
Arijit Sarkar <asarkar@retailershakti.com>
2025-10-30 14:51:42 +0530

    update

--- FrontEnd/lib/const/api_url_const.dart
+++ FrontEnd/lib/const/api_url_const.dart
@@ -3,13 +3,11 @@ import 'dart:math';
 import '../flavors.dart';
 
 class ApiUrlConst {
-  static String baseUrl = F.appFlavor == Flavor.prod
-      ? "https://www.googleapis.com/"
-      : F.appFlavor == Flavor.stg
-          ? "https://www.stg-googleapis.com/"
-          : F.appFlavor == Flavor.dev
-              ? "https://www.dev-googleapis.com/"
-              : "https://www.googleapis.com/";
+  static String baseUrl = F.appFlavor == Flavor.stg
+      ? "https://www.stg-googleapis.com/"
+      : F.appFlavor == Flavor.dev
+          ? "https://www.dev-googleapis.com/"
+          : "https://www.googleapis.com/";
 
   static String books = "${baseUrl}books/v1/volumes";
   static String hostUrl = "https://www.google.com";
