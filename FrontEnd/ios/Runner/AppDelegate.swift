fba598e8397a3fab946731144f3c5bcd8738dd46
Arijit Sarkar <asarkar@retailershakti.com>
2026-06-25 16:02:58 +0530

    update FlutterScene

--- FrontEnd/ios/Runner/AppDelegate.swift
+++ FrontEnd/ios/Runner/AppDelegate.swift
@@ -4,22 +4,21 @@ import flutter_local_notifications
 import app_links
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
         GeneratedPluginRegistrant.register(with: registry)
       }
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
-    if let url = AppLinks.shared.getLink(launchOptions: launchOptions) {
-       AppLinks.shared.handleLink(url: url)
-       return true
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
