057f4fbf49644c3418ff575bce9c14265f760e77
Arijit Sarkar <asarkar@retailershakti.com>
2026-07-20 17:00:16 +0530

    update showLoading

--- FrontEnd/lib/shared/ui/atoms/indicators/loading_widget.dart
+++ FrontEnd/lib/shared/ui/atoms/indicators/loading_widget.dart
@@ -6,7 +6,6 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:dbnus/shared/constants/color_const.dart';
 import 'package:dbnus/core/services/context_service.dart';
 
-
 class LoadingWidget extends StatelessWidget {
   const LoadingWidget({
     super.key,
@@ -54,7 +53,7 @@ class LoadingWidget extends StatelessWidget {
 /// ```dart
 /// // Show the loading indicator before starting an async task
 /// final dialogContext = await showLoading();
-/// 
+///
 /// try {
 ///   // Perform a long-running operation...
 ///   await fetchSomeData();
@@ -75,18 +74,22 @@ Future<BuildContext> showLoading() {
         dialogCompleter.complete(context);
       }
 
-      return Center(
-        child: SpinKitThreeBounce(
-          itemBuilder: (_, int index) {
-            return DecoratedBox(
-              decoration: BoxDecoration(
-                  color: index.isEven
-                      ? ColorConst.baseHexColor
-                      : ColorConst.baseHexColor.withValues(alpha: 0.2),
-                  borderRadius: const BorderRadius.all(Radius.circular(50))),
-            );
-          },
-          size: 30.0,
+      return PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (didPop, result) {},
+        child: Center(
+          child: SpinKitThreeBounce(
+            itemBuilder: (_, int index) {
+              return DecoratedBox(
+                decoration: BoxDecoration(
+                    color: index.isEven
+                        ? ColorConst.baseHexColor
+                        : ColorConst.baseHexColor.withValues(alpha: 0.2),
+                    borderRadius: const BorderRadius.all(Radius.circular(50))),
+              );
+            },
+            size: 30.0,
+          ),
         ),
       );
     },
@@ -105,7 +108,7 @@ Future<BuildContext> showLoading() {
 /// // 1. Hiding without a specific context (uses global context):
 /// showLoading();
 /// await performTask();
-/// hideLoading(); 
+/// hideLoading();
 ///
 /// // 2. Hiding with a specific context:
 /// final dialogContext = await showLoading();
