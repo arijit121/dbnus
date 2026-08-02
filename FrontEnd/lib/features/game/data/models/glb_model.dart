096d9eaf9b84354115827ce38f14f6312ef8ca83
arijit121 <ruarijitsarkar@gmail.com>
2026-08-02 23:24:01 +0530

    feat: Remove preset models from GlbModel for cleaner code structure

--- FrontEnd/lib/features/game/data/models/glb_model.dart
+++ FrontEnd/lib/features/game/data/models/glb_model.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import '../../domain/entities/glb_model_entity.dart';
 
 class GlbModel extends GlbModelEntity {
@@ -10,39 +9,4 @@ class GlbModel extends GlbModelEntity {
     required super.defaultScale,
     required super.themeColor,
   });
-
-  static const List<GlbModel> presetModels = [
-    GlbModel(
-      id: 'damaged_helmet',
-      name: 'Cyber Helmet',
-      description: 'Futuristic Sci-Fi Battle Helmet with PBR textures',
-      assetPath: 'assets/models/3d/damaged_helmet.glb',
-      defaultScale: 1.8,
-      themeColor: Color(0xFFFF0055),
-    ),
-    GlbModel(
-      id: 'duck',
-      name: 'Golden Duck',
-      description: 'Classic Khronos 3D Duck GLB Model',
-      assetPath: 'assets/models/3d/duck.glb',
-      defaultScale: 1.4,
-      themeColor: Color(0xFFFFCC00),
-    ),
-    GlbModel(
-      id: 'cesium_man',
-      name: 'Astronaut Explorer',
-      description: 'Rigged 3D Character Model',
-      assetPath: 'assets/models/3d/cesium_man.glb',
-      defaultScale: 2.0,
-      themeColor: Color(0xFF00F0FF),
-    ),
-    GlbModel(
-      id: 'avocado',
-      name: '3D Avocado',
-      description: 'Photogrammetry 3D Model',
-      assetPath: 'assets/models/3d/avocado.glb',
-      defaultScale: 15.0,
-      themeColor: Color(0xFF00FF66),
-    ),
-  ];
 }
